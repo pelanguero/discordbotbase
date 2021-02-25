301d6ae0f435db7a82d9ada452ad0f622e288974
pelanguero <pelanguero@gmail.com>
2021-02-24 23:11:54 -0500

    basico:player name

---
+++ main.py
@@ -0,0 +1,47 @@
+import discord
+import os
+import requests
+import json
+from discord.utils import get
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+playerUrl = "https://api.clashofclans.com/v1/players/%23"
+getHeader = {"authorization": "Bearer "+os.getenv("TOKEN-COC")}
+clan = "#VYUPLPP8"
+
+
+def jugador(idd):
+    responsee = requests.get(playerUrl+idd, headers=getHeader)
+    print(responsee)
+    return responsee.json()
+
+
+bot = commands.Bot(command_prefix='!!')
+
+
+@bot.event
+async def on_ready():
+    print("Logueado")
+
+
+@bot.command()
+async def player(ctx, arg):
+    print("se esta probando el jugador "+arg)
+    name = ""+arg
+    if name.startswith("#"):
+        pro = jugador(name[1:])
+    else:
+        pro = jugador(name)
+    await ctx.send("Nombre:"+pro["name"]+"/n"+"Nombre:"+pro["name"]+"/n")
+    pass
+
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
+#     if message.content.startswith("!coc:"):
+#         clashofclans(message)
+
+bot.run(os.getenv("TOKEN"))
