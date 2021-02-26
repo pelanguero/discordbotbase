ee521c723cb4701949c9706112000e852b212ee3
pelanguero <pelanguero@gmail.com>
2021-02-26 18:17:24 -0500

    register parcial

--- main.py
+++ main.py
@@ -27,6 +27,21 @@ async def on_ready():
 
 
 @bot.command()
+async def register(ctx, arg):
+    # intenta cambiar el rol del usuario segun el api de coc
+    naame = ""+arg
+    if naame.startswith("#"):
+        pro = jugador(naame[1:])
+    else:
+        pro = jugador(naame)
+    print("Asignando Usuario")
+    if pro["role"] == "leader":
+        rolee = get(ctx.guild.roles, name="Líder")
+        await ctx.author.add_roles(rolee)
+        await ctx.send(ctx.author.name+" ahora es de rango "+"Líder")
+
+
+@bot.command()
 async def player(ctx, arg):
     print("se esta probando el jugador "+arg)
     name = ""+arg
@@ -37,6 +52,11 @@ async def player(ctx, arg):
     await ctx.send("Nombre: "+pro["name"]+"\n"+"THlvl: "+str(pro["townHallLevel"])+"\n"+"BHlvl: "+str(pro["builderHallLevel"])+"\n"+"XPlvl: "+str(pro["expLevel"])+"\n")
     pass
 
+
+# @bot.command()
+# async def help(ctx, arg):
+#     if arg == "coc":
+#         bot.commands
 # @client.event
 # async def on_message(message):
 #     if message.author == client.user:
